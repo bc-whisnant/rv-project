ea055eae3f996018c4eaeef6ce4dbe93b5532e2d
Brandon Whisnant <bc.whisnant@gmail.com>
2018-04-04 20:40:02 -0400

    mocked up the upper portion of project with basic styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,193 @@
-// getting json data from dealers.json
+var dealersList = {
+  zipcode: "28205",
+  location: {
+    lat: 35.2263714,
+    lon: -80.7990185
+  },
+  dealers: [
+    {
+      data: {
+        companyID: 401929,
+        name: "Aqua Experts",
+        phone1: "1-888-234-5678",
+        email: "info@aquaexperts.com",
+        addressLine1: "1234 Some Street",
+        addressLine2: "",
+        city: "Charlotte",
+        state: "North Carolina",
+        country: "US",
+        zipcode: "28205",
+        weekHours: {
+          weekdays: "7:00am - 7:00pm",
+          sat: "7:00am - 3:00pm",
+          sun: ""
+        },
+        certifications: ["Installation Pro", "Residential Pro", "Service Pro"]
+      }
+    },
+    {
+      data: {
+        companyID: 401928,
+        name: "Premium Pools & Spas of Charlotte",
+        phone1: "1-888-888-8888",
+        email: "info@premiumpoolscharlotte.com",
+        addressLine1: "1234 Some Street",
+        addressLine2: "",
+        city: "Charlotte",
+        state: "North Carolina",
+        country: "US",
+        zipcode: "28205",
+        weekHours: {
+          weekdays: "7:00am - 7:00pm",
+          sat: "",
+          sun: "On Call"
+        },
+        certifications: [
+          "Installation Pro",
+          "Commercial Pro",
+          "Residential Pro",
+          "Service Pro"
+        ]
+      }
+    },
+    {
+      data: {
+        companyID: 401927,
+        name: "Carolina Pool Specialists",
+        phone1: "1-877-987-6543",
+        email: "info@cps.com",
+        addressLine1: "1234 Some Street",
+        addressLine2: "",
+        city: "Charlotte",
+        state: "North Carolina",
+        country: "US",
+        zipcode: "28205",
+        weekHours: {
+          weekdays: "8:00am - 5:00pm",
+          sat: "8:00am - 5:00pm",
+          sun: ""
+        },
+        certifications: [
+          "Installation Pro",
+          "Commercial Pro",
+          "Residential Pro",
+          "Service Pro"
+        ]
+      }
+    },
+    {
+      data: {
+        companyID: 401926,
+        name: "Pools'r'Us",
+        phone1: "1-888-346-4574",
+        email: "info@poolsrus.com",
+        addressLine1: "1234 Some Street",
+        addressLine2: "",
+        city: "Charlotte",
+        state: "North Carolina",
+        country: "US",
+        zipcode: "28205",
+        weekHours: {
+          mon: "7:00am - 7:00pm",
+          tue: "7:00am - 7:00pm",
+          wed: "7:00am - 7:00pm",
+          thu: "7:00am - 7:00pm",
+          fri: "7:00am - 7:00pm",
+          sat: "7:00am - 3:00pm",
+          sun: "1:00pm - 5:00pm"
+        },
+        certifications: [
+          "Installation Pro",
+          "Commercial Pro",
+          "Residential Pro"
+        ]
+      }
+    },
+    {
+      data: {
+        companyID: 401925,
+        name: "Clean Pool Systems",
+        phone1: "1-888-246-7485",
+        email: "info@cleanpools.com",
+        addressLine1: "1234 Some Street",
+        addressLine2: "",
+        city: "Charlotte",
+        state: "North Carolina",
+        country: "US",
+        zipcode: "28205",
+        weekHours: {
+          mon: "7:00am - 7:00pm",
+          tue: "7:00am - 7:00pm",
+          wed: "7:00am - 7:00pm",
+          thu: "7:00am - 7:00pm",
+          fri: "7:00am - 7:00pm",
+          sat: "7:00am - 3:00pm",
+          sun: ""
+        },
+        certifications: ["Installation Pro", "Residential Pro"]
+      }
+    },
+    {
+      data: {
+        companyID: 401924,
+        name: "Pool Service Pros",
+        phone1: "1-888-PRO-POOL",
+        email: "info@propool.com",
+        addressLine1: "1234 Some Street",
+        addressLine2: "",
+        city: "Charlotte",
+        state: "North Carolina",
+        country: "US",
+        zipcode: "28205",
+        weekHours: {
+          mon: "7:00am - 7:00pm",
+          tue: "7:00am - 7:00pm",
+          wed: "7:00am - 7:00pm",
+          thu: "7:00am - 7:00pm",
+          fri: "7:00am - 7:00pm",
+          sat: "7:00am - 3:00pm",
+          sun: ""
+        },
+        certifications: [
+          "Installation Pro",
+          "Commercial Pro",
+          "Residential Pro",
+          "Service Pro"
+        ]
+      }
+    },
+    {
+      data: {
+        companyID: 401923,
+        name: "Talent Pool",
+        phone1: "1-888-797-3463",
+        email: "info@talentpool.com",
+        addressLine1: "1234 Some Street",
+        addressLine2: "",
+        city: "Charlotte",
+        state: "North Carolina",
+        country: "US",
+        zipcode: "28205",
+        weekHours: {
+          mon: "7:00am - 7:00pm",
+          tue: "7:00am - 7:00pm",
+          wed: "7:00am - 7:00pm",
+          thu: "7:00am - 7:00pm",
+          fri: "7:00am - 7:00pm",
+          sat: "7:00am - 7:00pm",
+          sun: "7:00am - 7:00pm"
+        },
+        certifications: ["Installation Pro", "Commercial Pro", "Service Pro"]
+      }
+    }
+  ]
+};
 
-var dealerSection = document.querySelector('dealers-section')
+console.log(dealersList.dealers[0]);
+console.log(dealersList);
 
-var requestURL =
-  'https://github.com/bc-whisnant/rv-json-file/blob/master/dealers.json';
-var request = new XMLHttpRequest();
+// the following code dynamically renders content for the aqua experts section
+var aquaSection = document.querySelector('.aqua');
+aquaSection.querySelector('h2').innerHTML = 'hello there'
 
-request.open('GET', requestURL);
 
-request.responseType = 'json';
-
-request.send();
-
-request.onload = function() {
-    var dealers = request.response;
-    populateDealerSection(dealers);
-    showDealers(dealers);
-}
-
-function populateDealerSection(jsonObj) {
-    var dealersH1 = document.createElement('h1');
-    dealersH1.textContent = jsonObj(dealers.name);
-    dealerSection.appendChild(dealersH1);
-}
